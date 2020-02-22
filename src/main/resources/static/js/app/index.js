79a2a4fe561f8d894f81347a0e52ef7e963d9818
gim-ujae <kwj1270@naver.com>
2020-02-22 18:06:26 +0900

    4-3 게시글 등록 화면 만들기(수정)

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -14,7 +14,7 @@ var main = {
 
         $.ajax({
             type: 'POST',
-            url: '/api/vi/posts',
+            url: '/api/v1/posts',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
@@ -27,4 +27,4 @@ var main = {
     }
 };
 
-index.init()
+main.init()
